3a47be3d6a7f96cc710006cf3319656382e2ad79
Yay295 <Yay295@users.noreply.github.com>
2017-06-26 01:04:41 -0500

    update to V4

--- fitCurvesAlt.js
+++ fitCurvesAlt.js
@@ -24,69 +24,97 @@
 	- Unrolled some loops.
 	- "Inlined" some functions that were only called once.
 	Note: None of the V4 changes in the full version affected this version.
+	V5 Changes:
+	- Rebuilt from V4 of the main code. Restrictions on `points` input lifted.
+	- Simplified loops and other things in generateBezier().
+	- `maxError` was again removed for the same reason as before.
+	- Removed now unused math.js function sum().
+	- Inlined fitCubic() and generateBezier() because there's no recursion in this version.
+	- Removed unused functions bezier.qprime() and bezier.qprimeprime().
+	- Renamed bezier.q() to just bezier().
+	- Created local var `len` and removed now unneeded lodash function last().
 */
 
 /*
-	points - An array of points (ex. [[0,0],[0.25,5],[0.5,7],[0.75,3],[1,1]]) that reside on the
-	curve to fit.
-
+	points - An array of points (ex. [[0,0],[1,5],[3,7]]) that reside on the curve to fit.
 	return - An array of the four points required for a Cubic Bezier Curve.
 */
 function fitCurve(points) {
 	"use strict";
 
-	// math.js functions used in this function
+
+	// math.js functions used in this file
 	var add = (A,B) => [A[0]+B[0],A[1]+B[1]];
 	var subtract = (A,B) => [A[0]-B[0],A[1]-B[1]];
 	var multiply = (A,B) => [A[0]*B,A[1]*B];
 	var divide = (A,B) => [A[0]/B,A[1]/B];
 	var dot = (A,B) => A[0]*B[0]+A[1]*B[1];
 	var norm = A => Math.sqrt((A[0]*A[0])+(A[1]*A[1]));
-
-	var bezier = t => add(multiply([1,1], 3 * (1-t) * t * t), multiply([1,1], Math.pow(t,3)));
 	var normalize = v => divide(v,norm(v));
 
-	var leftTangent = normalize(points[1]);
-	var rightTangent = normalize(subtract(points[3],points[4]));
-	var i;
+	// Evaluates a cubic bezier at t. Returns a point.
+	function bezier(ctrlPoly,t) {
+		let tx = 1 - t;
+		return	add(add(multiply(ctrlPoly[0], tx * tx * tx), multiply(ctrlPoly[1], 3 * tx * tx * t)), add(multiply(ctrlPoly[2], 3 * tx * t * t), multiply(ctrlPoly[3], t * t * t)));
+	}
 
-	var u = [0,0,0,0,0];
-	u[1] = norm(subtract(points[1],points[0]));
-	u[2] = u[1] + norm(subtract(points[2],points[1]));
-	u[3] = u[2] + norm(subtract(points[3],points[2]));
-	u[4] = u[3] + norm(subtract(points[4],points[3]));
 
-	u[1] /= u[4];
-	u[2] /= u[4];
-	u[3] /= u[4];
-	u[4] = 1;
+	var len = points.length;
+	var leftTangent = normalize(subtract(points[1],points[0]));
+	var rightTangent = normalize(subtract(points[len-2],points[len-1]));
 
-	var A = [[0,0],[0,0]];
+
+	// Remove duplicate points and sanitize input.
+	points = points.filter((point,i) => (i === 0 || !(point[0] === points[i-1][0] && point[1] === points[i-1][1])));
+	if (len < 2) return [];
+	if (len === 2) {
+		var dist = norm(subtract(points[0], points[1])) / 3;
+		return [points[0], add(points[0], multiply(leftTangent, dist)), add(points[1], multiply(rightTangent, dist)), points[1]];
+	}
+
+
+	// Assign parameter values to digitized points using relative distances between points.
+	var u = [0];
+	for (let i = 1; i < len; ++i)
+		u.push(u[i-1] + norm(subtract(points[i],points[i-1])));
+	for (let i = 0; i < len; ++i)
+		u[i] /= u[len-1];
+
+
+	var bezCurve = [points[0], points[0], points[len-1], points[len-1]];
 	var C = [0,0,0,0];
 	var X = [0,0];
 
-	for (i = 1; i < 5; ++i) {
-		var p = u[i];
-		A[0] = multiply(leftTangent, 3 * (1 - p) * (1 - p) * p);
-		A[1] = multiply(rightTangent, 3 * (1 - p) * p * p);
+	for (let i = 0; i < len; ++i) {
+		var ui = u[i];
+		var ux = 1 - ui
+		var A = multiply(leftTangent, 3 * ux * ux * ui);
+		var B = multiply(rightTangent, 3 * ux * ui * ui);
 
-		C[0] += dot(A[0],A[0]);
-		C[1] += dot(A[0],A[1]);
-		C[2] += dot(A[0],A[1]);
-		C[3] += dot(A[1],A[1]);
+		C[0] += dot(A,A);
+		C[1] += dot(A,B);
+		C[2] += dot(A,B);
+		C[3] += dot(B,B);
 
-		var tmp = subtract(points[i], bezier(u[i]));
-		X[0] += dot(A[0],tmp);
-		X[1] += dot(A[1],tmp);
+		var tmp = subtract(points[i],bezier(bezCurve,ui));
+		X[0] += dot(A,tmp);
+		X[1] += dot(B,tmp);
 	}
 
-	var temp = C[0] * C[3] - C[2] * C[1];
-	var alpha_l = (X[0] * C[3] - X[1] * C[1]) / temp;
-	var alpha_r = (C[0] * X[1] - C[2] * X[0]) / temp;
-	var epsilon = 1.0e-6 * Math.sqrt(2);
+
+	var det_C0_C1 = (C[0] * C[3]) - (C[2] * C[1]);
+	var det_C0_X  = (C[0] * X[1]) - (C[2] * X[0]);
+	var det_X_C1  = (X[0] * C[3]) - (X[1] * C[1]);
+	var alpha_l = det_C0_C1 === 0 ? 0 : det_X_C1 / det_C0_C1;
+	var alpha_r = det_C0_C1 === 0 ? 0 : det_C0_X / det_C0_C1;
+	var segLength = norm(subtract(points[0], points[len-1]));
+	var epsilon = 1.0e-6 * segLength;
 
 	if (alpha_l < epsilon || alpha_r < epsilon)
-		alpha_l = alpha_r = Math.sqrt(2) / 3;
+		alpha_l = alpha_r = segLength / 3;
+	bezCurve[1] = add(bezCurve[0], multiply(leftTangent, alpha_l));
+	bezCurve[2] = add(bezCurve[3], multiply(rightTangent, alpha_r));
 
-	return [[0,0], multiply(leftTangent, alpha_l), add([1,1], multiply(rightTangent, alpha_r)), [1,1]];
+
+	return bezCurve;
 }
